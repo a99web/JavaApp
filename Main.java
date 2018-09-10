fad3135aaf2d5c03a1b129aab254bcd310813f6e
pauaskara@gmail.com <pauaskara@gmail.com>
2018-09-10 20:08:46 +0530

    added new functionality( insert, select, selectAll)

--- Main.java
+++ Main.java
@@ -1,8 +1,52 @@
+import java.io.Console;
+import java.util.Scanner;
 import java.sql.*;
 
+
 public class Main {
+
+    public static Scanner sc = new Scanner(System.in);
+    public static Console console = System.console();
+
     public static void main(String args[]){
-        SQLiteJDBC db = new SQLiteJDBC();
-        db.connect();    
+        SQLiteJDBC db = new SQLiteJDBC("passwords.db");
+
+        System.out.println("Please input any of the below options");
+        System.out.println("**********************************************************\n");
+        System.out.println("1: To insert new row into the table\n2: To display the data for a particular service\n3: Display all\n4: Exit\n");
+        System.out.println("**********************************************************\n");
+
+        while(true) {
+                String service_name;
+                System.out.print("Option: ");
+                int option = sc.nextInt();
+		switch(option){
+
+		    case 1:
+			 System.out.print("Please input the service name: ");
+			 service_name = sc.next();
+			 System.out.print("Please input the username: ");
+			 String username = sc.next();
+			 System.out.print("Please input the password: ");
+			 // console.readPassword returns char[] array
+			 String password = new String(console.readPassword());
+			 db.insert(service_name, username, password);
+                         break;
+		   case 2:
+			 System.out.print("Please enter the service name: ");
+			 service_name = sc.next();
+			 db.select(service_name);
+                         break;
+                   case 3:
+                         db.selectAll();
+                         break;
+		   case 4:
+			 System.exit(0);
+		   default:
+			 continue;
+		}
+
+      }
+
     }
 }
--- SQLiteJDBC.java
+++ SQLiteJDBC.java
@@ -2,18 +2,84 @@ import java.sql.*;
 
 public class SQLiteJDBC {
 
-    public static void connect() {
-        Connection conn = null;
+    private Connection conn = null;
+
+    public SQLiteJDBC(String database){
         try {
-              //Class.forName("org.sqlite.JDBC");
-              conn = DriverManager.getConnection("jdbc:sqlite:db/passwords.db");
+            conn = DriverManager.getConnection("jdbc:sqlite:db/" + database);
+            createTable("passwords");
+        }
+        catch ( Exception e ) {
+              System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+              System.exit(0);
+        }
+        System.out.println("Successfully connected to the database");
+    }
+
+   public void createTable(String table){
+       try {
               Statement statement = conn.createStatement();
               // creating the Password table
-              statement.execute("CREATE table passwords (of TEXT, username TEXT, pswd TEXT)");
+              statement.execute("CREATE table IF NOT EXISTS passwords (service TEXT, username TEXT, password TEXT)");
         } catch ( Exception e ) {
               System.err.println( e.getClass().getName() + ": " + e.getMessage() );
               System.exit(0);
         }
-        System.out.println("Opened database successfully");
+        System.out.println("Created Table " + table + " successfully");
    }
+
+   public void insert(String service, String username, String password){
+        
+        String sql = "INSERT INTO passwords(service,username,password) VALUES(?,?,?)";
+ 
+        try {
+                PreparedStatement pstmt = conn.prepareStatement(sql);
+                pstmt.setString(1, service);
+                pstmt.setString(2, username);
+                pstmt.setString(3, password);
+                pstmt.executeUpdate();
+        } catch (SQLException e) {
+                System.out.println(e.getMessage());
+        }
+                
+   }
+
+   public void select(String service_name){
+     
+        String sql = "SELECT * FROM passwords WHERE service='" + service_name +"'";
+        
+        try {
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql);
+            
+            // loop through the result set
+            while (rs.next()) {
+                System.out.println(rs.getString("service") +  "\t" + 
+                                   rs.getString("username") + "\t" +
+                                   rs.getString("password"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+   public void selectAll(){
+
+        String sql = "SELECT * FROM passwords";
+
+        try {
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql);
+
+            // loop through the result set
+            while (rs.next()) {
+                System.out.println(rs.getString("service") +  "\t" +
+                                   rs.getString("username") + "\t" +
+                                   rs.getString("password"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
 }
